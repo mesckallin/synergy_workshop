d59c5812a6344b6e119371dd92ac8d5fda9247e0
mesckallin <66205639+mesckallin@users.noreply.github.com>
2024-04-01 19:06:16 +0500

    Create task_2.py

---
+++ input_print/task_2.py
@@ -0,0 +1,8 @@
+print('Введите правильную последовательность 4 этапов развития человека, выбрав из вариантов: homo sapiens, australopithecus, homo erectus, dryopithecus.')
+
+stage_1 = input('Этап 1: ')
+stage_2 = input('Этап 2: ')
+stage_3 = input('Этап 3: ')
+stage_4 = input('Этап 4: ')
+
+print(stage_1, stage_2, stage_3, stage_4, sep='=>')
